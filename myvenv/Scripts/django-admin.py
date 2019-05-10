8324a0ad2e06ba7ca71f32fe678eba875e75ecc7
Jeongah002 <jeongah002@gmail.com>
2019-05-10 22:59:07 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\김증댕\desktop\likekion ja\selfproject\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
